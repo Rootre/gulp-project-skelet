15fee1ea2587ad584c20764564f2d0827cd0e7f4
Ondřej Tůma <ondrej@footshop.cz>
2017-01-04 10:37:06 +0100

    aktualizace gulpfile: newer na minifikaci obrazku, intervaly u watchingu na zvyseni vykonu, pokus o babel, pridani excluded slozky ve scriptech, kam se davaji soubory co nespadaji do balicku

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp');
 
-var themeDir = "",
-	siteUrl = "vbv.tt";
+var baseDir = "themes/default-bootstrap/",
+	siteUrl = "footshop.tt";
 
 var paths = {
-	assetsDir: 		themeDir + "assets/",
-	distDir: 		themeDir + "dist/",
-	imagesAssets: 	themeDir + "assets/images/",
-	imagesDist: 	themeDir + "dist/images/",
-	jsAssets: 		themeDir + "assets/js/",
-	jsDist: 		themeDir + "dist/js/",
-	cssAssets: 		themeDir + "assets/sass/",
-	cssDist: 		themeDir + "dist/css/",
-	iconsAssets: 	themeDir + "assets/icons/",
+	assetsDir: 		baseDir + "assets/",
+	distDir: 		baseDir + "dist/",
+	imagesAssets: 	baseDir + "assets/images/",
+	imagesDist: 	baseDir + "dist/images/",
+	jsAssets: 		baseDir + "assets/js/",
+	jsDist: 		baseDir + "dist/js/",
+	cssAssets: 		baseDir + "assets/sass/",
+	cssDist: 		baseDir + "dist/css/",
+	iconsAssets: 	baseDir + "assets/icons/",
 };
 
 var jshint = require('gulp-jshint');
@@ -25,13 +25,16 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant'); // $ npm i -D imagemin-pngquant
 var csso = require('gulp-csso');
 var cache = require('gulp-cached');
+var sort = require('gulp-sort');
 var gutil = require('gulp-util');
 var CssImporter = require('node-sass-css-importer')({
 	import_paths: [ paths.iconsAssets ]
 });
 var postcss = require('gulp-postcss');
-var sort = require('gulp-sort');
 var rucksack = require('rucksack-css');
+var babel = require('gulp-babel');
+var debug = require('gulp-debug');
+var newer = require('gulp-newer');
 
 
 var browserSync = require('browser-sync').create();
@@ -47,6 +50,7 @@ gulp.task('lint', function() {
 gulp.task('imagemin', function() {
 	return gulp.src([paths.imagesAssets + "**/*", "!" + paths.imagesAssets + "{sprites,sprites/**}"])
 		.pipe(cache('imagining'))
+		.pipe(newer(paths.imagesDist))
 		.pipe(imagemin({
 			progressive: true,
 			optimizationLevel: 3,
@@ -72,7 +76,7 @@ gulp.task('sprites', function() {
 	var spriteData = gulp.src(paths.imagesAssets + "sprites/*.png").pipe(spritesmith({
 		imgName: 'sprite.png',
 		cssName: '_sprite.scss',
-		imgPath: '../images/sprite.png' + (new Date().getTime())
+		imgPath: '../images/sprite.png?' + (new Date().getTime()),	//this fixes cache problems
 	}));
 
 	spriteData.img.pipe(gulp.dest(paths.imagesDist)); // output path for the sprite
@@ -81,20 +85,29 @@ gulp.task('sprites', function() {
 	return spriteData;
 });
 
-// Concatenate & Minify JS
+// Concatenate js files
 gulp.task('scripts', function() {
-	return gulp.src(paths.jsAssets + "*.js")
+	return gulp.src([paths.jsAssets + "**/*.js", "!" + paths.jsAssets + "{excluded,excluded/**}"])
 		.pipe(sort({
 			asc: true
 		}))
 		.pipe(concat('all.js'))
+		// .pipe(babel({
+		// 	presets: ['es2015']
+		// }))
 		.pipe(gulp.dest(paths.jsDist))
 		.pipe(browserSync.stream());
 });
-// Build production scripts
+// Concatenate and minify js files
 gulp.task('scripts-build', function() {
-	return gulp.src(paths.jsAssets + "*.js")
+	return gulp.src([paths.jsAssets + "**/*.js", "!" + paths.jsAssets + "{excluded,excluded/**}"])
+		.pipe(sort({
+			asc: true
+		}))
 		.pipe(concat('all.min.js'))
+		// .pipe(babel({
+		// 	presets: ['es2015']
+		// }))
 		.pipe(uglify().on('error', gutil.log))
 		.pipe(gulp.dest(paths.jsDist));
 });
@@ -115,16 +128,16 @@ gulp.task('watch', function() {
 		proxy: siteUrl
 	});
 
-	gulp.watch(paths.jsAssets + "**/*.js", ['scripts']);
-	gulp.watch(paths.cssAssets + "**/*.scss", ['sass']);
-	gulp.watch(paths.imagesAssets + "sprites/*.png", ['sprites']);
-	gulp.watch([paths.imagesAssets + "**/*", "!" + paths.imagesAssets + "{sprites,sprites/**}"], ['imagemin']);
+	gulp.watch(paths.jsAssets + "**/*.js", { interval: 500 }, ['scripts']);
+	gulp.watch(paths.cssAssets + "**/*.scss", { interval: 500 }, ['sass']);
+	gulp.watch(paths.imagesAssets + "sprites/*.png", { interval: 1000 }, ['sprites']);
+	gulp.watch([paths.imagesAssets + "**/*", "!" + paths.imagesAssets + "{sprites,sprites/**}"], { interval: 1000 }, ['imagemin']);
 
-	gulp.watch("{*,app/pages/*}.{php,html}").on("change", browserSync.reload);
+	gulp.watch("{*,app/pages/*}.{php,html}", { interval: 1000 }).on("change", browserSync.reload);
 });
 
 
-gulp.task('build', ['sass', 'csso', 'scripts-build', 'sprites', 'imagemin']);
+gulp.task('build', ['sass', 'csso', 'scripts-build']);
 
 // Default Task
 gulp.task('default', ['sass', 'scripts', 'watch']);
