eba6b775a3c3e137672ecbb5e65feac8aad53e1b
Ondřej Tůma <ondrej@footshop.cz>
2017-04-06 12:55:50 +0200

    preparing for lang redirection

--- index.php
+++ index.php
@@ -5,5 +5,12 @@ require_once "app/bootstrap.php";
 
 $app = APP::getInstance();
 
+/* redirecting to langs
+if (!preg_match("/^(cs|en)\/(.*)/", ltrim($_SERVER['REQUEST_URI'], '/'))) {
+	header("Location: " . $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . "/" . APP::getLang() . "/" . ltrim($_SERVER['REQUEST_URI'], '/'));
+	die();
+}
+*/
+
 $app->render();
 ?>
